be2107c68ce914fd51d888980d16b04367f238a3
Nahuel Lautaro Jimenez <nahuellautaro.jimenez@estudiantes.unahur.edu.ar>
2025-08-28 17:02:57 -0300

    Hice primera parte

--- src/filtros/validaciones.js
+++ src/filtros/validaciones.js
@@ -1,1 +1,28 @@
+const validaciones = [
+  {
+    nombre: "stock",
+    funcion: (product, value) => {
+      return product.stock > value;
+    }
+  },
+  {
+    nombre: "precio",
+    funcion: (product, value) => {
+      return product.precio <= value;
+    }
+  },
+  {
+    nombre: "nombre",
+    funcion: (product, value) => {
+      return product.nombre.length < value;
+    }
+  },
+  {
+    nombre: "categoria",
+    funcion: (product, value) => {
+      return value.includes(product.categorias);
+    }
+  }
+]
 
+module.exports = validaciones;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
+const express = require("express");
+const validaciones = require("./filtros/validaciones.js")
+const app = express()
+const productos = require("../data/productos.json")
 
+app.use(express.json());
+
+const PORT = 3001;
+
+const filtrar = (funciones) => {
+  return productos.filter((p) => funciones.every((f) => getValidacion(f).funcion(p,f.values)));
+};
+
+const getValidacion = (funcion) => {
+  const validacion = validaciones.find((v) => v.nombre === funcion.fn);
+  return validacion;
+};
+
+/*
+console.log(getValidacion(  {
+    "fn": "stock",
+    "values": 10
+  }))
+
+  console.log(getValidacion(  {
+    "fn": "categoria",
+    "values": ["electrodomestico", "deportivo"]
+  }))
+*/
+
+app.post("/productos/filtrar", (req, res) => {
+  const data = req.body;
+  const respuesta = filtrar(data);
+  res.status(200).json(respuesta);
+});
+
+app.get("/productos",(req,res) => {;
+  res.status(200).json(productos);
+});
+
+app.listen(PORT, () => {
+  console.log(`App iniciada en puerto ${PORT}`);
+})
